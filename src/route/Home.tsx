b23f5a1b3f51588455c285defb0743073b303193
iqbal <ifachry@rocketmail.com>
2025-05-07 09:01:44 +0700

    fix

--- src/route/Home.tsx
+++ src/route/Home.tsx
@@ -83,12 +83,6 @@ const Home = () => {
                     >
                       Read Story
                     </button>
-                    <button
-                      onClick={() => navigate(`/tipWriters`)}
-                      className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600 text-sm"
-                    >
-                      Tip
-                    </button>
                   </div>
                 </div>
               ))
